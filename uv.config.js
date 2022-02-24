18035fb9d5e89fd8c961f5bfcb87fc70b7aca329
Caracal.js <59297610+caracal-js@users.noreply.github.com>
2022-02-24 16:51:55 -0500

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/sw/',
-    bare: 'https://incog.dev/bare/',
+    bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv.handler.js',
