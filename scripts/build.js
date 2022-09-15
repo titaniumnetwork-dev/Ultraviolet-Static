214359561335ee7765efcea53f531fcea0a0068c
David Reed <reedswork@proton.me>
2022-09-15 18:39:00 -0400

    add and run prettier & eslint

--- scripts/build.js
+++ scripts/build.js
@@ -1,15 +1,15 @@
 /**
- * Copies public & ultraviolet into the dist directory.
+ * Copies src & ultraviolet into the dist directory.
  */
 
-import fs from 'fs-extra';
-import { uvPath } from 'ultraviolet';
+import fs from "fs-extra";
+import { uvPath } from "ultraviolet";
 
 try {
-	await fs.mkdir('dist');
+  await fs.mkdir("dist");
 } catch (err) {
-	if (err?.code !== 'EEXIST') throw err;
+  if (err?.code !== "EEXIST") throw err;
 }
 
-await fs.copy(uvPath, 'dist/uv/');
-await fs.copy('public', 'dist/');
+await fs.copy(uvPath, "dist/uv/");
+await fs.copy("src", "dist/");
