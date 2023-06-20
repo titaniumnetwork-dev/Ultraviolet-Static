efeae66746db17556d7e351defc65c0e57f02792
David Reed <reedswork@proton.me>
2023-06-20 22:39:40 +0300

    only throw if navigator.serviceWorker is undefined

--- public/register-sw.js
+++ public/register-sw.js
@@ -5,7 +5,7 @@
 const stockSW = "/uv/sw.js";
 
 /**
- * List of hostnames that are allowed to run serviceworkers on http:
+ * List of hostnames that are allowed to run serviceworkers on http://
  */
 const swAllowedHostnames = ["localhost", "127.0.0.1"];
 
@@ -14,14 +14,15 @@ const swAllowedHostnames = ["localhost", "127.0.0.1"];
  * Used in 404.html and index.html
  */
 async function registerSW() {
-  if (
-    location.protocol !== "https:" &&
-    !swAllowedHostnames.includes(location.hostname)
-  )
-    throw new Error("Service workers cannot be registered without https.");
+  if (!navigator.serviceWorker) {
+    if (
+      location.protocol !== "https:" &&
+      !swAllowedHostnames.includes(location.hostname)
+    )
+      throw new Error("Service workers cannot be registered without https.");
 
-  if (!navigator.serviceWorker)
     throw new Error("Your browser doesn't support service workers.");
+  }
 
   // Ultraviolet has a stock `sw.js` script.
   await navigator.serviceWorker.register(stockSW, {
